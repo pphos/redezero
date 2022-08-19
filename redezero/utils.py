ec57c7aa7c3e6058fad67beaf149f5080b760d74
phos <phos@yuta-zenbook.localdomain>
2022-08-19 23:00:35 +0900

    [feat]計算グラフ描画用の処理追加

--- redezero/utils.py
+++ redezero/utils.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
+from pathlib import Path
 from typing import cast
+import subprocess
 import numpy as np
 
-from redezero import types, variable
+from redezero import types
+from redezero import variable
+from redezero import function
 
 
 def force_array(x: np.ndarray | types.ScalarValue) -> np.ndarray:
@@ -46,3 +50,122 @@ def force_operand_value(x: types.ScalarValue | types.OperandValue) -> types.Oper
         converted_x = force_array(x)
 
     return converted_x
+
+
+# =============================================================================
+# Visualize for computational graph
+# =============================================================================
+def _dot_var(v: variable.Variable, verbose: bool = False) -> str:
+    """VariableをDOT言語の文字列へ変換する
+
+    Parameters
+    ----------
+    v : Variable
+        DeZeroの変数
+    verbose : bool, optional
+        ndarrayインスタンスの「形状」と「型」も合わせて出力するフラグ
+
+    Returns
+    -------
+    str
+        Variableインスタンスの情報をDOT言語に変換した文字列
+    """
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        if v.name is not None:
+            name += ': '
+        name += f'{str(v.shape)} {str(v.dtype)}'
+
+    return f'{id(v)} [label="{name}", color=orange, style=filled]\n'
+
+
+def _dot_func(f: function.Function) -> str:
+    """FunctionをDOT言語の文字列へ変換する
+
+    Parameters
+    ----------
+    f : Function
+        DeZeroの関数
+
+    Returns
+    -------
+    str
+        Functionインスタンスの情報をDOT言語に変換した文字列
+    """
+    txt = f'{id(f)} [label="{f.__class__.__name__}", color=lightblue, style=filled, shape=box]\n'
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y()))  # yはweakref
+
+    return txt
+
+
+def get_dot_graph(output: variable.Variable, verbose: bool = True) -> str:
+    """計算グラフからDOT言語へ変換する
+
+    Parameters
+    ----------
+    output : Variable
+        計算グラフの最終出力変数
+    verbose : bool, optional
+        ndarrayインスタンスの「形状」と「型」も合わせて出力するフラグ
+
+    Returns
+    -------
+    str
+        計算グラフの情報をDOT言語に変換した文字列
+    """
+    txt = ''
+    funcs: list[function.Function] = []
+    seen_set = set()
+
+    def add_func(f: function.Function) -> None:
+        """関数の追加
+        """
+        if f not in seen_set:
+            funcs.append(f)
+            seen_set.add(f)
+
+    add_func(cast(function.Function, output.creator))
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                add_func(x.creator)
+
+    return 'digraph g {\n' + txt + '}'
+
+
+def plot_dot_graph(output: variable.Variable, verbose: bool = True, to_file: str = 'graph.png') -> None:
+    """計算グラフをGraphvizで画像に変換する
+
+    Parameters
+    ----------
+    output : Variable
+        計算グラフの最終出力変数
+    verbose : bool, optional
+        ndarrayインスタンスの「形状」と「型」も合わせて出力するフラグ
+    to_file : str, optional
+        保存する画像のファイル名
+    """
+    # 計算グラフをDOT言語に変換
+    dot_graph = get_dot_graph(output, verbose)
+
+    # dotデータをファイルに保存
+    tmp_dir = Path('.dezero')
+    if not tmp_dir.exists():
+        tmp_dir.mkdir()
+    graph_path = Path(tmp_dir).joinpath('tmp_graph.dot')
+    Path(graph_path).write_text(dot_graph)
+
+    # osのdotコマンド呼び出し
+    extension = Path(to_file).suffix.lstrip('.')
+    cmd = f'dot {graph_path} -T {extension} -o {to_file}'
+    subprocess.run(cmd, shell=True)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import pytest
+import shutil
 import numpy as np
+from pathlib import Path
 
 from redezero import utils
 from redezero import Variable
@@ -44,3 +46,102 @@ def params_for_force_operand_value():
 def test_正常系_force_operand_value(x):
     converted_x = utils.force_operand_value(x)
     assert isinstance(converted_x, Variable) | isinstance(converted_x, np.ndarray)
+
+
+# =============================================================================
+# Visualize for computational graph
+# =============================================================================
+def params_for_dot_var():
+    """正常系_Variableインスタンス情報をDOT言語に変換のパラメータ
+    """
+    x = Variable(np.array([[1, 2, 3], [4, 5, 6]]), name='x')
+
+    return {
+        '入力変数の形状も出力する': (
+            x,
+            True,
+            f'{id(x)} [label="x: (2, 3) int64", color=orange, style=filled]\n'
+        ),
+        '入力変数の形状を出力しない': (
+            x,
+            False,
+            f'{id(x)} [label="x", color=orange, style=filled]\n'
+        )
+    }
+
+
+@pytest.mark.parametrize(
+    'x, verbose, expect_msg',
+    params_for_dot_var().values(),
+    ids=params_for_dot_var().keys()
+)
+def test_正常系_Variableインスタンス情報をDOT言語に変換(x, verbose, expect_msg):
+    assert utils._dot_var(x, verbose=verbose) == expect_msg
+
+
+def test_正常系_Functionインスタンス情報をDOT言語に変換():
+    # Functionインスタンスのセットアップ
+    x0 = Variable(np.array(1.0))
+    x1 = Variable(np.array(1.0))
+    y = x0 + x1
+
+    # 期待するDOT言語の文字列
+    expect = f'{id(y.creator)} [label="Add", color=lightblue, style=filled, shape=box]\n'
+    expect += f'{id(x0)} -> {id(y.creator)}\n'
+    expect += f'{id(x1)} -> {id(y.creator)}\n'
+    expect += f'{id(y.creator)} -> {id(y)}\n'
+
+    assert utils._dot_func(y.creator) == expect
+
+
+def test_正常系_計算グラフからDOT言語に変換():
+    x0 = Variable(np.array(1.0), name='x0')
+    x1 = Variable(np.array(1.0), name='x1')
+    y = x0 + x1
+    y.name = 'y'
+
+    # 期待するDOT言語の文字列
+    expect = 'digraph g {\n'
+    expect += f'{id(y)} [label="y", color=orange, style=filled]\n'
+    expect += f'{id(y.creator)} [label="Add", color=lightblue, style=filled, shape=box]\n'
+    expect += f'{id(x0)} -> {id(y.creator)}\n'
+    expect += f'{id(x1)} -> {id(y.creator)}\n'
+    expect += f'{id(y.creator)} -> {id(y)}\n'
+    expect += f'{id(x0)} [label="x0", color=orange, style=filled]\n'
+    expect += f'{id(x1)} [label="x1", color=orange, style=filled]\n'
+    expect += '}'
+
+    assert utils.get_dot_graph(y, verbose=False) == expect
+
+
+@pytest.fixture
+def dot_graph():
+    """plot_dot_graph用のfixture
+    """
+    # SetUp
+    output_dir = Path('.dezero')
+    img = {
+        'actual': output_dir.joinpath('goldstein.png'),
+        'expect': Path('tests/resources/image/goldstein.png')
+    }
+    yield img
+
+    # 生成した計算グラフの削除
+    if img['actual'].exists():
+        shutil.rmtree(output_dir)
+
+
+def test_正常系_計算グラフをGraphvizで画像に変換(dot_graph):
+    def goldstein(x, y) -> Variable:
+        """サンプル用のGolestein-Price関数
+        """
+        z = (1 + (x + y + 1)**2 * (19 - 14 * x + 3 * x**2 - 14 * y + 6 * x * y + 3 * y**2)) * \
+            (30 + (2 * x - 3 * y)**2 * (18 - 32 * x + 12 * x**2 + 48 * y - 36 * x * y + 27 * y**2))
+        return z
+
+    # 計算グラフの作成
+    x = Variable(np.array(1.0), name='x')
+    y = Variable(np.array(1.0), name='y')
+    z = goldstein(x, y)
+    z.backward()
+    z.name = 'z'
